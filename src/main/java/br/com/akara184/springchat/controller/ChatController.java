4eb3e6ee72a47058121fffb4ea46301c1414270c
akara184 <davisuporterj@gmail.com>
2026-01-24 00:06:45 -0300

    init

---
+++ src/main/java/br/com/akara184/springchat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package br.com.akara184.springchat.controller;
+
+import java.util.Map;
+
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final GoogleGenAiChatModel chatModel;
+
+    @Autowired
+    public ChatController(GoogleGenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+}
